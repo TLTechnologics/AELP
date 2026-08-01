79ce44e6582e8941f67bd16f39c3c7aa3f1e1e33
icyflame06 <icyflame06@gmail.com>
2026-08-01 22:08:28 +0530

    Fix CORS regex literal

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:3000", 
         "https://aelp-dszy.vercel.app"
     ],
-    allow_origin_regex="https://.*\.vercel\.app",
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
