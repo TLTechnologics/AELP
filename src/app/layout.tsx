babe2bf98a4ec1571b514004c5d8a5f38c2c86fd
icyflame06 <icyflame06@gmail.com>
2026-08-21 17:11:46 +0530

    Fix Eruda loading using next/script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import type { Metadata } from "next";
 import { Anton } from "next/font/google";
 import "./globals.css";
@@ -27,8 +28,9 @@ export default function RootLayout({
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap" rel="stylesheet" />
       </head>
       <body className={`${anton.variable} font-sans antialiased bg-background text-foreground bg-grid-pattern min-h-screen flex flex-col`}>
-        <script src="//cdn.jsdelivr.net/npm/eruda" async></script>
-        <script dangerouslySetInnerHTML={{ __html: 'window.onload = function() { if (typeof eruda !== "undefined") eruda.init(); }' }}></script>
+        <Script src="https://cdn.jsdelivr.net/npm/eruda" strategy="beforeInteractive" />
+        <Script id="eruda-init" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: 'if (typeof eruda !== "undefined") eruda.init();' }} />
+
 
         <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light" disableTransitionOnChange>
           <QueryProvider>
